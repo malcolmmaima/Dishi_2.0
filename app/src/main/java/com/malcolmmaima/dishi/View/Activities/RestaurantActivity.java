d8e7707285da18b2d348e8185faa9c8c659daf41
malcolmmaima <malcolmmaima@gmail.com>
2020-01-17 16:14:15 +0300

    navigation drawer implementation on restaurant activity

--- app/src/main/java/com/malcolmmaima/dishi/View/Activities/RestaurantActivity.java
+++ app/src/main/java/com/malcolmmaima/dishi/View/Activities/RestaurantActivity.java
@@ -1,28 +1,39 @@
 package com.malcolmmaima.dishi.View.Activities;
 
 import android.app.ActivityOptions;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
 import com.malcolmmaima.dishi.R;
 import com.malcolmmaima.dishi.View.Fragments.CustomerOrderFragment;
 import com.malcolmmaima.dishi.View.Fragments.HomeFragment;
 import com.malcolmmaima.dishi.View.Fragments.OrdersFragment;
 import com.malcolmmaima.dishi.View.Fragments.ProfileFragment;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.annotation.NonNull;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class RestaurantActivity extends AppCompatActivity {
+public class RestaurantActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
 
     FloatingActionButton addMenu;
 
@@ -55,6 +66,10 @@ public class RestaurantActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurant);
         addMenu = findViewById(R.id.button_add_menu);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        setTitle("");
 
         BottomNavigationView navView = findViewById(R.id.nav_view);
         navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
@@ -91,6 +106,86 @@ public class RestaurantActivity extends AppCompatActivity {
 
         //Used to select an item programmatically
         navView.getMenu().getItem(2).setChecked(true);
+
+        /**
+         * Navigation drawer
+         */
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.drawer_nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        //Drawer header
+        View headerView = navigationView.getHeaderView(0);
+        final TextView navUsername = headerView.findViewById(R.id.userName);
+        final TextView emailAddress = headerView.findViewById(R.id.emailaddress);
+
+        //Set header data
+        navUsername.setText("Malcolm Maima");
+        emailAddress.setText("malcolmmaima@gmail.com");
+
     }
 
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+        Fragment fragment = null;
+        final Class[] fragmentClass = {null};
+
+        if (id == R.id.menu1) {
+            Toast.makeText(this, "Clicked!", Toast.LENGTH_SHORT).show();
+        }
+
+        else if (id == R.id.menu2) {
+            Toast.makeText(this, "Clicked!", Toast.LENGTH_SHORT).show();
+        }
+
+        else if (id == R.id.menu3) {
+            Toast.makeText(this, "Clicked!", Toast.LENGTH_SHORT).show();
+        } //else if (id == R.id.menu4) {
+        //fragmentClass[0] = FragmentFour.class;
+        //}
+
+        else if (id == R.id.nav_share) {
+            Toast.makeText(RestaurantActivity.this, "Clicked!", Toast.LENGTH_LONG).show();
+
+        } else if (id == R.id.nav_send) {
+            Toast.makeText(this, "Clicked!", Toast.LENGTH_SHORT).show();
+        }
+
+        try {
+            fragment = (Fragment) fragmentClass[0].newInstance();
+
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            fragmentManager.beginTransaction().replace(R.id.flContent, fragment).commit();
+
+            DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+            drawer.closeDrawer(GravityCompat.START);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return true;
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
+    }
 }
