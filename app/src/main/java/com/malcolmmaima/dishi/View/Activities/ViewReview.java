34f54a6cc2ed7bd248d10d460e50fa46f581fc2f
malcolmmaima <malcolmmaima@gmail.com>
2020-04-25 01:00:08 +0300

    handle large like/comment counts in ViewReview

--- app/src/main/java/com/malcolmmaima/dishi/View/Activities/ViewReview.java
+++ app/src/main/java/com/malcolmmaima/dishi/View/Activities/ViewReview.java
@@ -47,6 +47,7 @@ import com.malcolmmaima.dishi.View.Adapter.ReviewReplyAdapter;
 import com.squareup.picasso.Picasso;
 
 import java.io.IOException;
+import java.text.DecimalFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -410,8 +411,49 @@ public class ViewReview extends AppCompatActivity implements SwipeRefreshLayout.
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 try {
-                    int totalLikes = (int) dataSnapshot.getChildrenCount();
-                    likesTotal.setText("" + totalLikes);
+                    int total = (int) dataSnapshot.getChildrenCount();
+                    Double totalLikes = Double.valueOf(total);
+
+                    //below 1000
+                    if(totalLikes < 1000){
+                        DecimalFormat value = new DecimalFormat("#");
+                        likesTotal.setText(""+value.format(totalLikes));
+                    }
+
+                    // 1000 to 999,999
+                    else if(totalLikes >= 1000 && totalLikes <= 999999){
+                        if(totalLikes % 1000 == 0){ //No remainder
+                            DecimalFormat value = new DecimalFormat("#####");
+                            likesTotal.setText(""+value.format(total/1000)+"K");
+                        }
+
+                        else { //Has remainder 999.9K
+                            DecimalFormat value = new DecimalFormat("######.#");
+                            Double divided = totalLikes/1000;
+                            if(value.format(divided).equals("1000")){
+                                likesTotal.setText("1M"); //if rounded off
+                            } else {
+                                likesTotal.setText(""+value.format(divided)+"K");
+                            }
+                        }
+                    }
+
+                    // 1,000,0000 to 999,999,999
+                    else if(totalLikes >= 1000000 && totalLikes <= 999999999){
+                        if(totalLikes % 1000000 == 0) { //No remainder
+                            DecimalFormat value = new DecimalFormat("#");
+                            likesTotal.setText(""+value.format(totalLikes/1000000)+"M");
+                        }
+
+                        else { //Has remainder 9.9M, 999.9M etc
+                            DecimalFormat value = new DecimalFormat("#.#");
+                            if(value.format(totalLikes/1000000).equals("1000")){
+                                likesTotal.setText("1B"); //if rounded off
+                            } else {
+                                likesTotal.setText(""+value.format(totalLikes/1000000)+"M");
+                            }
+                        }
+                    }
                 } catch (Exception e){}
             }
 
@@ -426,8 +468,49 @@ public class ViewReview extends AppCompatActivity implements SwipeRefreshLayout.
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 try {
-                    int totalComments = (int) dataSnapshot.getChildrenCount();
-                    commentsTotal.setText("" + totalComments);
+                    int total = (int) dataSnapshot.getChildrenCount();
+                    Double totalComments = Double.valueOf(total);
+
+                    //below 1000
+                    if(totalComments < 1000){
+                        DecimalFormat value = new DecimalFormat("#");
+                        commentsTotal.setText(""+value.format(totalComments));
+                    }
+
+                    // 1000 to 999,999
+                    else if(totalComments >= 1000 && totalComments <= 999999){
+                        if(totalComments % 1000 == 0){ //No remainder
+                            DecimalFormat value = new DecimalFormat("#####");
+                            commentsTotal.setText(""+value.format(total/1000)+"K");
+                        }
+
+                        else { //Has remainder 999.9K
+                            DecimalFormat value = new DecimalFormat("######.#");
+                            Double divided = totalComments/1000;
+                            if(value.format(divided).equals("1000")){
+                                commentsTotal.setText("1M"); //if rounded off
+                            } else {
+                                commentsTotal.setText(""+value.format(divided)+"K");
+                            }
+                        }
+                    }
+
+                    // 1,000,0000 to 999,999,999
+                    else if(totalComments >= 1000000 && totalComments <= 999999999){
+                        if(totalComments % 1000000 == 0) { //No remainder
+                            DecimalFormat value = new DecimalFormat("#");
+                            commentsTotal.setText(""+value.format(totalComments/1000000)+"M");
+                        }
+
+                        else { //Has remainder 9.9M, 999.9M etc
+                            DecimalFormat value = new DecimalFormat("#.#");
+                            if(value.format(totalComments/1000000).equals("1000")){
+                                commentsTotal.setText("1B"); //if rounded off
+                            } else {
+                                commentsTotal.setText(""+value.format(totalComments/1000000)+"M");
+                            }
+                        }
+                    }
                 } catch (Exception e){}
             }
 
