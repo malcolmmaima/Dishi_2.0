06f60cd5515b41538341c10acd6bf61d42f475b7
malcolmmaima <malcolmmaima@gmail.com>
2020-04-29 14:09:24 +0300

    followed you notification in module implemented

--- app/src/main/java/com/malcolmmaima/dishi/View/Activities/MyNotifications.java
+++ app/src/main/java/com/malcolmmaima/dishi/View/Activities/MyNotifications.java
@@ -1,20 +1,54 @@
 package com.malcolmmaima.dishi.View.Activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatImageView;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.malcolmmaima.dishi.Model.NotificationModel;
+import com.malcolmmaima.dishi.Model.UserModel;
 import com.malcolmmaima.dishi.R;
+import com.malcolmmaima.dishi.View.Adapter.NotificationAdapter;
+import com.malcolmmaima.dishi.View.Adapter.ProductAdapter;
 
-public class MyNotifications extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class MyNotifications extends AppCompatActivity implements SwipeRefreshLayout.OnRefreshListener {
+
+    String myPhone;
+    FirebaseUser user;
+    DatabaseReference notificationsRef;
+    ValueEventListener notificationListener;
+    RecyclerView recyclerView;
+    TextView emptyTag;
+    AppCompatImageView icon;
+    SwipeRefreshLayout mSwipeRefreshLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_notifications);
 
+        icon = findViewById(R.id.menuIcon);
+        recyclerView = findViewById(R.id.rview);
+        emptyTag = findViewById(R.id.empty_tag);
         Toolbar topToolBar = findViewById(R.id.toolbar);
         setSupportActionBar(topToolBar);
 
@@ -22,7 +56,6 @@ public class MyNotifications extends AppCompatActivity {
         getSupportActionBar().setDisplayShowHomeEnabled(true);
 
         setTitle("Notifications");
-
         //Back button on toolbar
         topToolBar.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
@@ -30,5 +63,102 @@ public class MyNotifications extends AppCompatActivity {
                 finish(); //Go back to previous activity
             }
         });
+
+        // SwipeRefreshLayout
+        mSwipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swipe_container);
+        mSwipeRefreshLayout.setOnRefreshListener(MyNotifications.this);
+        mSwipeRefreshLayout.setColorSchemeResources(R.color.colorPrimary,
+                android.R.color.holo_green_dark,
+                android.R.color.holo_orange_dark,
+                android.R.color.holo_blue_dark);
+
+        mSwipeRefreshLayout.post(new Runnable() {
+
+            @Override
+            public void run() {
+
+                mSwipeRefreshLayout.setRefreshing(true);
+
+                // Fetching data from server
+                fetchNotifications();
+            }
+        });
+
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        myPhone = user.getPhoneNumber(); //Current logged in user phone number
+        notificationsRef = FirebaseDatabase.getInstance().getReference("notifications/"+myPhone);
+
+    }
+
+    private void fetchNotifications() {
+        notificationListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                List<NotificationModel> notifications = new ArrayList<>();
+                if(!dataSnapshot.exists()){
+                    mSwipeRefreshLayout.setRefreshing(false);
+
+                    NotificationAdapter recycler = new NotificationAdapter(MyNotifications.this, notifications);
+                    RecyclerView.LayoutManager layoutmanager = new LinearLayoutManager(MyNotifications.this);
+                    recyclerView.setLayoutManager(layoutmanager);
+                    recyclerView.setItemAnimator(new DefaultItemAnimator());
+                    recyclerView.setAdapter(recycler);
+                    emptyTag.setVisibility(View.VISIBLE);
+                    icon.setVisibility(View.VISIBLE);
+                }
+
+                else {
+                    for(DataSnapshot notif : dataSnapshot.getChildren()){
+                        NotificationModel notification = notif.getValue(NotificationModel.class);
+                        notifications.add(notification);
+
+                        if (!notifications.isEmpty()) {
+
+                            mSwipeRefreshLayout.setRefreshing(false);
+                            //Collections.reverse(list);
+                            NotificationAdapter recycler = new NotificationAdapter(MyNotifications.this, notifications);
+                            RecyclerView.LayoutManager layoutmanager = new LinearLayoutManager(MyNotifications.this);
+                            recyclerView.setLayoutManager(layoutmanager);
+                            recyclerView.setItemAnimator(new DefaultItemAnimator());
+                            recycler.notifyDataSetChanged();
+                            recyclerView.setAdapter(recycler);
+                            emptyTag.setVisibility(View.INVISIBLE);
+                            icon.setVisibility(View.INVISIBLE);
+                        } else {
+
+                            mSwipeRefreshLayout.setRefreshing(false);
+
+                            NotificationAdapter recycler = new NotificationAdapter(MyNotifications.this, notifications);
+                            RecyclerView.LayoutManager layoutmanager = new LinearLayoutManager(MyNotifications.this);
+                            recyclerView.setLayoutManager(layoutmanager);
+                            recyclerView.setItemAnimator(new DefaultItemAnimator());
+                            recyclerView.setAdapter(recycler);
+                            emptyTag.setVisibility(View.VISIBLE);
+                            icon.setVisibility(View.VISIBLE);
+
+                        }
+                    }
+                }
+
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        };
+        notificationsRef.addValueEventListener(notificationListener);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        notificationsRef.removeEventListener(notificationListener);
+    }
+
+    @Override
+    public void onRefresh() {
+        fetchNotifications();
     }
 }
---
+++ app/src/main/java/com/malcolmmaima/dishi/View/Adapter/NotificationAdapter.java
@@ -0,0 +1,374 @@
+package com.malcolmmaima.dishi.View.Adapter;
+
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.app.ActivityOptions;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.PorterDuff;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.widget.AppCompatButton;
+import androidx.appcompat.widget.AppCompatImageView;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.firestore.auth.User;
+import com.malcolmmaima.dishi.Controller.GetCurrentDate;
+import com.malcolmmaima.dishi.Controller.TimeAgo;
+import com.malcolmmaima.dishi.Model.NotificationModel;
+import com.malcolmmaima.dishi.Model.UserModel;
+import com.malcolmmaima.dishi.R;
+import com.malcolmmaima.dishi.View.Activities.Chat;
+import com.malcolmmaima.dishi.View.Activities.ViewCustomerOrder;
+import com.malcolmmaima.dishi.View.Activities.ViewImage;
+import com.malcolmmaima.dishi.View.Activities.ViewProfile;
+import com.squareup.picasso.Picasso;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+public class NotificationAdapter extends RecyclerView.Adapter<NotificationAdapter.MyHolder>{
+    String TAG = "NotificationAdapter";
+    Context context;
+    List<NotificationModel> listdata;
+    long DURATION = 200;
+    UserModel userData;
+    String myPhone;
+    FirebaseUser user;
+
+    public NotificationAdapter(Context context, List<NotificationModel> listdata) {
+        this.listdata = listdata;
+        this.context = context;
+    }
+
+
+    @Override
+    public NotificationAdapter.MyHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.card_notification,parent,false);
+
+        NotificationAdapter.MyHolder myHolder = new NotificationAdapter.MyHolder(view);
+        return myHolder;
+    }
+
+    public void onBindViewHolder(final NotificationAdapter.MyHolder holder, final int position) {
+        final NotificationModel my_notification = listdata.get(position);
+
+        /**
+         * Adapter animation
+         */
+        setAnimation(holder.itemView, position);
+
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        myPhone = user.getPhoneNumber(); //Current logged in user phone number
+
+        holder.followUnfollow.setVisibility(View.GONE);
+        holder.contact_name.setText("loading...");
+        DatabaseReference userDetails = FirebaseDatabase.getInstance().getReference("users/"+my_notification.getFrom());
+        DatabaseReference followingRef = FirebaseDatabase.getInstance().getReference("following/"+myPhone+"/"+my_notification.getFrom());
+        userDetails.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                userData = dataSnapshot.getValue(UserModel.class);
+                holder.contact_name.setText(userData.getFirstname()+" "+userData.getLastname());
+
+                /**
+                 * Load image url onto imageview
+                 */
+                try {
+                    //Load food image
+                    Picasso.with(context).load(userData.getProfilePic()).fit().centerCrop()
+                            .placeholder(R.drawable.default_profile)
+                            .error(R.drawable.default_profile)
+                            .into(holder.profilePic);
+                } catch (Exception e){
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        //Get today's date
+        GetCurrentDate currentDate = new GetCurrentDate();
+        String currDate = currentDate.getDate();
+
+        //Get date status update was posted
+        String dtEnd = currDate;
+        String dtStart = my_notification.getTimeStamp();
+
+        //https://stackoverflow.com/questions/8573250/android-how-can-i-convert-string-to-date
+        //Format both current date and date status update was posted
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd:HH:mm:ss:Z");
+        try {
+
+            //Convert String date values to Date values
+            Date dateStart;
+            Date dateEnd;
+
+            //Date dateStart = format.parse(dtStart);
+            String[] timeS = Split(my_notification.getTimeStamp());
+            String[] timeT = Split(currDate);
+
+            /**
+             * timeS[0] = date
+             * timeS[1] = hr
+             * timeS[2] = min
+             * timeS[3] = seconds
+             * timeS[4] = timezone
+             */
+
+            //post timeStamp
+            if(timeS[4].equals("EAT")){ //Noticed some devices post timezone like so ... i'm going to optimize for EA first
+                timeS[4] = "GMT+03:00";
+
+                //2020-04-27:20:37:32:GMT+03:00
+                dtStart = timeS[0]+":"+timeS[1]+":"+timeS[2]+":"+timeS[3]+":"+timeS[4];
+                dateStart = format.parse(dtStart);
+            } else {
+                dateStart = format.parse(dtStart);
+            }
+
+            //my device current date
+            if(timeT[4].equals("EAT")){ //Noticed some devices post timezone like so ... i'm going to optimize for EA first
+                timeT[4] = "GMT+03:00";
+
+                //2020-04-27:20:37:32:GMT+03:00
+                dtEnd = timeT[0]+":"+timeT[1]+":"+timeT[2]+":"+timeT[3]+":"+timeT[4];
+                dateEnd = format.parse(dtEnd);
+            } else {
+                dateEnd = format.parse(dtEnd);
+            }
+
+            //https://memorynotfound.com/calculate-relative-time-time-ago-java/
+            //Now compute timeAgo duration
+            TimeAgo timeAgo = new TimeAgo();
+
+            holder.notificationTime.setText(timeAgo.toRelative(dateStart, dateEnd, 1));
+
+        } catch (ParseException e) {
+            e.printStackTrace();
+            Log.d(TAG, "timeStamp: "+ e.getMessage());
+        }
+
+        //Show follow button if type of notification is 'followedwall'
+        if(my_notification.getType().equals("followedwall")){
+            holder.followUnfollow.setVisibility(View.VISIBLE);
+            holder.notificationMessage.setText(my_notification.getMessage());
+            followingRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    if(!dataSnapshot.exists()){
+                        holder.followUnfollow.getBackground().setColorFilter(context.getResources().getColor(R.color.colorPrimary), PorterDuff.Mode.MULTIPLY);
+                        holder.followUnfollow.setText("FOLLOW");
+                    }
+                    else {
+                        holder.followUnfollow.getBackground().setColorFilter(context.getResources().getColor(R.color.colorPrimaryDark), PorterDuff.Mode.MULTIPLY);
+                        holder.followUnfollow.setText("UNFOLLOW");
+                    }
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                }
+            });
+        }
+
+        /**
+         * Click listener on our card
+         */
+        holder.cardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View v) {
+                if(my_notification.getType().equals("followedwall") && !myPhone.equals(my_notification.getFrom())){
+                    Intent slideactivity = new Intent(context, ViewProfile.class)
+                            .setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+
+                    slideactivity.putExtra("phone", my_notification.getFrom());
+                    Bundle bndlanimation =
+                            ActivityOptions.makeCustomAnimation(context, R.anim.animation,R.anim.animation2).toBundle();
+                    context.startActivity(slideactivity, bndlanimation);
+                }
+            }
+        });
+
+        holder.followUnfollow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DatabaseReference followerRef = FirebaseDatabase.getInstance().getReference("followers/"+my_notification.getFrom()+"/"+myPhone);
+                followingRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        if(!dataSnapshot.exists()){
+                            followingRef.setValue("follow").addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void aVoid) {
+                                    followerRef.setValue("follow").addOnSuccessListener(new OnSuccessListener<Void>() {
+                                        @Override
+                                        public void onSuccess(Void aVoid) {
+                                            holder.followUnfollow.getBackground().setColorFilter(context.getResources().getColor(R.color.colorPrimaryDark), PorterDuff.Mode.MULTIPLY);
+                                            holder.followUnfollow.setText("UNFOLLOW");
+                                        }
+                                    });
+
+                                }
+                            }).addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    holder.followUnfollow.getBackground().setColorFilter(context.getResources().getColor(R.color.colorPrimary), PorterDuff.Mode.MULTIPLY);
+                                    holder.followUnfollow.setText("FOLLOW");
+                                }
+                            });
+                        }
+                        else {
+                            followingRef.removeValue().addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void aVoid) {
+                                    followerRef.removeValue().addOnSuccessListener(new OnSuccessListener<Void>() {
+                                        @Override
+                                        public void onSuccess(Void aVoid) {
+                                            holder.followUnfollow.getBackground().setColorFilter(context.getResources().getColor(R.color.colorPrimary), PorterDuff.Mode.MULTIPLY);
+                                            holder.followUnfollow.setText("FOLLOW");
+                                        }
+                                    });
+
+                                }
+                            }).addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    holder.followUnfollow.getBackground().setColorFilter(context.getResources().getColor(R.color.colorPrimaryDark), PorterDuff.Mode.MULTIPLY);
+                                    holder.followUnfollow.setText("UNFOLLOW");
+                                }
+                            });
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+            }
+        });
+
+
+        /**
+         * View image click listener
+         */
+        holder.profilePic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    Intent slideactivity = new Intent(context, ViewImage.class)
+                            .setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    slideactivity.putExtra("imageURL", userData.getProfilePic());
+                    context.startActivity(slideactivity);
+                } catch (Exception e){
+                    Log.e(TAG, "onClick: ", e);
+                }
+            }
+        });
+
+        holder.contact_name.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!myPhone.equals(my_notification.getFrom())){
+                    Intent slideactivity = new Intent(context, ViewProfile.class)
+                            .setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+
+                    slideactivity.putExtra("phone", my_notification.getFrom());
+                    Bundle bndlanimation =
+                            ActivityOptions.makeCustomAnimation(context, R.anim.animation,R.anim.animation2).toBundle();
+                    context.startActivity(slideactivity, bndlanimation);
+                }
+            }
+        });
+
+    }
+
+    /**
+     * @https://medium.com/better-programming/android-recyclerview-with-beautiful-animations-5e9b34dbb0fa
+     */
+    private void setAnimation(View itemView, int i) {
+        boolean on_attach = true;
+        if(!on_attach){
+            i = -1;
+        }
+        boolean isNotFirstItem = i == -1;
+        i++;
+        itemView.setAlpha(0.f);
+        AnimatorSet animatorSet = new AnimatorSet();
+        ObjectAnimator animator = ObjectAnimator.ofFloat(itemView, "alpha", 0.f, 0.5f, 1.0f);
+        ObjectAnimator.ofFloat(itemView, "alpha", 0.f).start();
+        animator.setStartDelay(isNotFirstItem ? DURATION / 2 : (i * DURATION / 3));
+        animator.setDuration(500);
+        animatorSet.play(animator);
+        animator.start();
+    }
+
+    public int getItemCount() {
+        return listdata.size();
+    }
+
+    class MyHolder extends RecyclerView.ViewHolder{
+        TextView contact_name, notificationMessage, notificationTime;
+        ImageView profilePic;
+        LinearLayout cardView;
+        AppCompatButton followUnfollow;
+
+        public MyHolder(View itemView) {
+            super(itemView);
+            contact_name = itemView.findViewById(R.id.contact_name);
+            profilePic = itemView.findViewById(R.id.user_dp);
+            cardView = itemView.findViewById(R.id.card_view);
+            notificationMessage = itemView.findViewById(R.id.message);
+            followUnfollow = itemView.findViewById(R.id.followUnfollow);
+            notificationTime = itemView.findViewById(R.id.notificationTime);
+
+            //Long Press
+            itemView.setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View v) {
+
+                    //Do something
+                    return false;
+                }
+            });
+
+        }
+    }
+
+    public String[] Split(String timeStamp){
+
+        String[] arrSplit = timeStamp.split(":");
+
+        return arrSplit;
+    }
+
+}
