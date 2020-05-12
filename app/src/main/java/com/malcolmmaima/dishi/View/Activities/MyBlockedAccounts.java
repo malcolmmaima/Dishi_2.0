f18f527ebb8e27a4394eb1b85aa8c3ee24d3fcfa
malcolmmaima <malcolmmaima@gmail.com>
2020-05-12 22:59:07 +0300

    MyBlockedUsers complete

--- app/src/main/java/com/malcolmmaima/dishi/View/Activities/MyBlockedAccounts.java
+++ app/src/main/java/com/malcolmmaima/dishi/View/Activities/MyBlockedAccounts.java
@@ -2,7 +2,12 @@ package com.malcolmmaima.dishi.View.Activities;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatImageView;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -16,16 +21,26 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.malcolmmaima.dishi.Controller.Fonts.MyTextView_Roboto_Medium;
+import com.malcolmmaima.dishi.Model.UserModel;
 import com.malcolmmaima.dishi.R;
+import com.malcolmmaima.dishi.View.Adapter.BlockedUserAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import io.fabric.sdk.android.services.common.SafeToast;
 
-public class MyBlockedAccounts extends AppCompatActivity {
+public class MyBlockedAccounts extends AppCompatActivity implements SwipeRefreshLayout.OnRefreshListener {
 
     String myPhone;
-    DatabaseReference myRef;
+    DatabaseReference myRef, myBlockedUsers;
     FirebaseUser user;
     FirebaseAuth mAuth;
+    AppCompatImageView icon;
+    MyTextView_Roboto_Medium emptyTag;
+    SwipeRefreshLayout mSwipeRefreshLayout;
+    RecyclerView recyclerview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +55,20 @@ public class MyBlockedAccounts extends AppCompatActivity {
 
         setTitle("Blocked Accounts");
 
+        emptyTag = findViewById(R.id.empty_tag);
+        emptyTag.setVisibility(View.GONE);
+        recyclerview = findViewById(R.id.rview);
+
+        icon = findViewById(R.id.blockedIcon);
+        icon.setVisibility(View.GONE);
+        // SwipeRefreshLayout
+        mSwipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swipe_container);
+        mSwipeRefreshLayout.setOnRefreshListener(this);
+        mSwipeRefreshLayout.setColorSchemeResources(R.color.colorPrimary,
+                android.R.color.holo_green_dark,
+                android.R.color.holo_orange_dark,
+                android.R.color.holo_blue_dark);
+
         mAuth = FirebaseAuth.getInstance();
         if(mAuth.getInstance().getCurrentUser() == null){
             finish();
@@ -47,6 +76,7 @@ public class MyBlockedAccounts extends AppCompatActivity {
         } else {
             user = FirebaseAuth.getInstance().getCurrentUser();
             myPhone = user.getPhoneNumber();
+            myBlockedUsers = FirebaseDatabase.getInstance().getReference("blocked/"+myPhone);
             myRef = FirebaseDatabase.getInstance().getReference("users/"+myPhone);
             myRef.child("pin").addListenerForSingleValueEvent(new ValueEventListener() {
                 @Override
@@ -78,6 +108,20 @@ public class MyBlockedAccounts extends AppCompatActivity {
 
                 }
             });
+
+            /**
+             * Showing Swipe Refresh animation on activity create
+             * As animation won't start on onCreate, post runnable is used
+             */
+            mSwipeRefreshLayout.post(new Runnable() {
+
+                @Override
+                public void run() {
+
+                    mSwipeRefreshLayout.setRefreshing(true);
+                    fetchBlocked();
+                }
+            });
         }
 
         //Back button on toolbar
@@ -88,6 +132,73 @@ public class MyBlockedAccounts extends AppCompatActivity {
             }
         });
 
+    }
+
+    private void fetchBlocked() {
+        mSwipeRefreshLayout.setRefreshing(true);
+        List<UserModel> blockedUsers = new ArrayList<>();
+        myBlockedUsers.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if(!dataSnapshot.exists()){
+                    mSwipeRefreshLayout.setRefreshing(false);
+                    BlockedUserAdapter recycler = new BlockedUserAdapter(MyBlockedAccounts.this, blockedUsers);
+                    RecyclerView.LayoutManager layoutmanager = new LinearLayoutManager(MyBlockedAccounts.this);
+                    recyclerview.setLayoutManager(layoutmanager);
+                    recyclerview.setItemAnimator(new DefaultItemAnimator());
+                    recyclerview.setAdapter(recycler);
+                    emptyTag.setVisibility(View.VISIBLE);
+                    icon.setVisibility(View.VISIBLE);
+                }
+
+                else {
+                    for(DataSnapshot blockedUser : dataSnapshot.getChildren()){
+                        DatabaseReference userDetails = FirebaseDatabase.getInstance().getReference("users/"+blockedUser.getKey());
+                        userDetails.addListenerForSingleValueEvent(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                                UserModel userFound = dataSnapshot.getValue(UserModel.class);
+                                userFound.setPhone(blockedUser.getKey());
+                                blockedUsers.add(userFound);
+
+
+                                if (!blockedUsers.isEmpty()) {
+                                    mSwipeRefreshLayout.setRefreshing(false);
+                                    //Collections.reverse(orders);
+                                    BlockedUserAdapter recycler = new BlockedUserAdapter(MyBlockedAccounts.this, blockedUsers);
+                                    RecyclerView.LayoutManager layoutmanager = new LinearLayoutManager(MyBlockedAccounts.this);
+                                    recyclerview.setLayoutManager(layoutmanager);
+                                    recyclerview.setItemAnimator(new DefaultItemAnimator());
+                                    recycler.notifyDataSetChanged();
+                                    recyclerview.setAdapter(recycler);
+                                    emptyTag.setVisibility(View.GONE);
+                                    icon.setVisibility(View.GONE);
+                                } else {
+                                    mSwipeRefreshLayout.setRefreshing(false);
+                                    BlockedUserAdapter recycler = new BlockedUserAdapter(MyBlockedAccounts.this, blockedUsers);
+                                    RecyclerView.LayoutManager layoutmanager = new LinearLayoutManager(MyBlockedAccounts.this);
+                                    recyclerview.setLayoutManager(layoutmanager);
+                                    recyclerview.setItemAnimator(new DefaultItemAnimator());
+                                    recyclerview.setAdapter(recycler);
+                                    emptyTag.setVisibility(View.GONE);
+                                    icon.setVisibility(View.GONE);
+                                }
+                            }
+
+                            @Override
+                            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                            }
+                        });
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 
     @Override
@@ -123,5 +234,10 @@ public class MyBlockedAccounts extends AppCompatActivity {
 
             }
         });
+    }
+
+    @Override
+    public void onRefresh() {
+        fetchBlocked();
     }
 }
