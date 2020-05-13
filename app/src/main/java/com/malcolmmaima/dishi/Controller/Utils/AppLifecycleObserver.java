6269b23a368522e7a03ce5d5f91158b140dc4ae8
malcolmmaima <malcolmmaima@gmail.com>
2020-05-13 17:43:21 +0300

    bugfix

--- app/src/main/java/com/malcolmmaima/dishi/Controller/Utils/AppLifecycleObserver.java
+++ app/src/main/java/com/malcolmmaima/dishi/Controller/Utils/AppLifecycleObserver.java
@@ -1,15 +1,24 @@
 package com.malcolmmaima.dishi.Controller.Utils;
 
+import android.content.Intent;
 import android.util.Log;
 
+import androidx.annotation.NonNull;
 import androidx.lifecycle.Lifecycle;
 import androidx.lifecycle.LifecycleObserver;
 import androidx.lifecycle.OnLifecycleEvent;
 
+import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.malcolmmaima.dishi.R;
+import com.malcolmmaima.dishi.View.Activities.AccountSettings;
+import com.malcolmmaima.dishi.View.Activities.SecurityPin;
 
 public class AppLifecycleObserver implements LifecycleObserver {
 
@@ -23,8 +32,7 @@ public class AppLifecycleObserver implements LifecycleObserver {
     @OnLifecycleEvent(Lifecycle.Event.ON_START)
     public void onEnterForeground() {
         //run the code we need
-        //lockApp(false);
-        Log.d(TAG, "onEnterForeground: true");
+        //Log.d(TAG, "onEnterForeground: true");
     }
 
     @OnLifecycleEvent(Lifecycle.Event.ON_STOP)
@@ -44,7 +52,20 @@ public class AppLifecycleObserver implements LifecycleObserver {
             myPhone = user.getPhoneNumber(); //Current logged in user phone number
 
             myRef = FirebaseDatabase.getInstance().getReference("users/"+myPhone);
-            myRef.child("appLocked").setValue(lockedApp);
+
+            myRef.child("pin").addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    if(dataSnapshot.exists()){
+                        myRef.child("appLocked").setValue(lockedApp);
+                    }
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                }
+            });
         }
     }
 }
