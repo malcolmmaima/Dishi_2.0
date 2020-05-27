e3886c17380f4eee16441094882c213c785fa94c
malcolmmaima <malcolmmaima@gmail.com>
2020-05-27 18:28:26 +0300

    Terms & Conditions / Privacy Policy generated

--- app/src/main/java/com/malcolmmaima/dishi/View/Activities/PrivacyPolicy.java
+++ app/src/main/java/com/malcolmmaima/dishi/View/Activities/PrivacyPolicy.java
@@ -1,16 +1,38 @@
 package com.malcolmmaima.dishi.View.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.text.Html;
+import android.view.View;
+import android.webkit.WebView;
+import android.widget.TextView;
 
 import com.malcolmmaima.dishi.R;
 
 public class PrivacyPolicy extends AppCompatActivity {
-
+    WebView privacyPolicy;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_privacy_policy);
+        privacyPolicy = findViewById(R.id.webView);
+
+        Toolbar topToolBar = findViewById(R.id.toolbar);
+        setSupportActionBar(topToolBar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        setTitle("Privacy Policy");
+
+        topToolBar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish(); //Go back to previous activity
+            }
+        });
     }
 }
