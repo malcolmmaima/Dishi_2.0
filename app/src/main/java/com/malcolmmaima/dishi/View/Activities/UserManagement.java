7f9886e28f8ad754f41c8a89c10b1612e10dbc25
malcolmmaima <malcolmmaima@gmail.com>
2020-06-08 17:34:00 +0300

    setup support dashboard

---
+++ app/src/main/java/com/malcolmmaima/dishi/View/Activities/UserManagement.java
@@ -0,0 +1,16 @@
+package com.malcolmmaima.dishi.View.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.malcolmmaima.dishi.R;
+
+public class UserManagement extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_management);
+    }
+}
